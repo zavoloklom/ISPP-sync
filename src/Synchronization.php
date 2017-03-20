1295faeb44c25c8e5786ab33ed37d5b267d2d90e
Sergey Kupletsky <s.kupletsky@gmail.com>
2017-03-20 04:28:32 +0300

    Refactor Synchronization Class

--- src/Synchronization.php
+++ src/Synchronization.php
@@ -18,8 +18,16 @@ use Pixie\QueryBuilder\QueryBuilderHandler;
 class Synchronization
 {
 
-  /** @var SlackNotification */
-  public $notify;
+  const ACTION_GROUPS = 'groups';
+  const ACTION_STUDENTS = 'students';
+  const ACTION_EVENTS = 'events';
+  const ACTION_ALL = 'all';
+
+  /** @var boolean */
+  public $notificationEnabled = false;
+
+  /** @var array */
+  public $config;
 
   /** @var QueryBuilderHandler */
   private $local_connection;
@@ -27,49 +35,107 @@ class Synchronization
   /** @var QueryBuilderHandler */
   private $web_connection;
 
+  /** @var SlackNotification */
+  private $notify;
+
   /**
    * Synchronization constructor.
    * @param array $config
    */
-  public function __construct(array $config)
+  public function __construct(array $config = [])
   {
     // Инициализация уведомлений
-    $this->notify = new SlackNotification($config);
-
-    // Установка соединения с локальным сервером
-    $localConnect = false;
-    try {
-      $local_connection = new Connection($config['local_server']['adapter'], $config['local_server']['options'], 'local');
-      $this->local_connection = new QueryBuilderHandler($local_connection);
-      echo 'Соединение с сервером ИС ПП успешно установлено'.PHP_EOL;
-      $localConnect = true;
-    } catch (\Exception $e) {
-      echo 'Не удалось установить соединение с сервером ИС ПП: ',  $e->getMessage(), ''.PHP_EOL;
+    if (array_key_exists('slack', $config) && is_array($config['slack']) && $this->notify = new SlackNotification($config['slack'])) {
+      $this->notificationEnabled = true;
     }
 
-    // Установка соединения с веб сервером
-    $serverConnect = false;
+    if (!$this->config) {$this->config = $config;}
+  }
+
+  /**
+   * @return QueryBuilderHandler
+   */
+  public function getLocalConnection(): QueryBuilderHandler
+  {
+    return $this->local_connection;
+  }
+
+  /**
+   * @return QueryBuilderHandler
+   */
+  public function getWebConnection(): QueryBuilderHandler
+  {
+    return $this->web_connection;
+  }
+
+  /**
+   * Установка соединения с локальным сервером ИС ПП
+   *
+   * @param array $config
+   * @param string $alias
+   * @return bool
+   */
+  private function setupLocalConnection(array $config, string $alias = NULL)
+  {
     try {
-      $web_connection = new Connection($config['web_server']['adapter'], $config['web_server']['options'], 'web');
-      $this->web_connection = new QueryBuilderHandler($web_connection);
-      echo 'Соединение с веб сервером успешно установлено'.PHP_EOL;
-      $serverConnect = true;
+      $connection = new Connection($config['adapter'], $config['options'], $alias);
+      $this->local_connection = new QueryBuilderHandler($connection);
+      echo 'Соединение с сервером ИС ПП успешно установлено', PHP_EOL;
+      return true;
+    } catch (\Exception $e) {
+      echo 'Не удалось установить соединение с сервером ИС ПП: ',  $e->getMessage(), PHP_EOL;
+      return false;
+    }
+  }
+
+  /**
+   * Установка соединения с веб сервером
+   *
+   * @param array $config
+   * @param string $alias
+   * @return bool
+   */
+  private function setupWebConnection(array $config, string $alias = NULL)
+  {
+    try {
+      $connection = new Connection($config['adapter'], $config['options'], $alias);
+      $this->web_connection = new QueryBuilderHandler($connection);
+      echo 'Соединение с веб сервером успешно установлено', PHP_EOL;
+      return true;
     } catch (\Exception $e) {
       echo 'Не удалось установить соединение с веб сервером: ',  $e->getMessage(), PHP_EOL;
+      return false;
     }
-
-    if (($localConnect && $serverConnect) === false) {
-      $this->notify->connectionError($localConnect, $serverConnect);
-      exit('Синхронизация невозможна'.PHP_EOL);
-    };
   }
+
+  /**
+   * Установка соединения с веб сервером и сервером ИС ПП
+   *
+   * @param array $config
+   * @throws \Exception
+   */
+  private function setupConnections(array $config)
+  {
+    $localConnect  = array_key_exists('local_server', $config) ? $this->setupLocalConnection($config['local_server']) : false;
+    $serverConnect = array_key_exists('web_server', $config)   ? $this->setupWebConnection($config['web_server']) : false;
+    if (($localConnect && $serverConnect) === false) {
+      if ($this->notificationEnabled) {
+        $this->notify->connectionError($localConnect, $serverConnect);
+      }
+      throw new \Exception('Синхронизация невозможна.');
+    }
+  }
+
 
   /**
    * Groups synchronization
    */
   public function groups()
   {
-    echo '\nСинхронизация идентификаторов групп'.PHP_EOL;
+    // Установка соединений
+    $this->setupConnections($this->config);
+
+    echo 'Синхронизация идентификаторов групп', PHP_EOL, PHP_EOL;
 
     $query = $this->local_connection
       ->table('clients_groups')
@@ -89,17 +155,30 @@ class Synchronization
       }
     }
 
-    echo '\n\n Количество групп '.$query->count();
+    echo 'Количество групп ', $query->count(), PHP_EOL;
 
+    echo 'Синхронизация идентификаторов групп выполнена', PHP_EOL;
+  }
+
+  private function getGroupInfo()
+  {
+    // Не надо
+  }
+
+  private function updateGroupInfo()
+  {
+
+  }
+
+  private function updateSyncInfo($table, $name)
+  {
     // Запись в таблицу синхронизаций
     try {
       //$this->serverDb->createCommand()->insert('{{%ispp_sync}}', ['action'=>'update-groups', 'errors'=> $errors, 'datetime'=>date('Y-m-d H:i:s')])->execute();
-      echo '\nЗапись в таблицу синхронизаций прошла успешно';
+      echo 'Запись в таблицу синхронизаций прошла успешно', PHP_EOL;
     } catch (\Exception $e) {
-      echo '\nЗапись в таблицу синхронизаций не удалась';
+      echo 'Запись в таблицу синхронизаций не удалась', PHP_EOL;
     }
-
-    echo '\nСинхронизация идентификаторов групп выполнена'.PHP_EOL;
   }
 
   /**
@@ -107,7 +186,7 @@ class Synchronization
    */
   public function students()
   {
-    echo 'Students Action';
+    return 'Students Action';
   }
 
   /**
@@ -126,6 +205,21 @@ class Synchronization
     $this->groups();
     $this->students();
     $this->events();
+  }
+
+  /**
+   * Return array of public actions for CLI
+   *
+   * @return array
+   */
+  public static function actionsArray()
+  {
+    return [
+      self::ACTION_GROUPS,
+      self::ACTION_STUDENTS,
+      self::ACTION_EVENTS,
+      self::ACTION_ALL
+    ];
   }
 
 }
