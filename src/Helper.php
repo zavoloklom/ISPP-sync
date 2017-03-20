7aa17db9349c0d45b05d5d13379e440b9de43436
Sergey Kupletsky <s.kupletsky@gmail.com>
2017-03-20 04:27:16 +0300

    Refactor Helper Class

--- src/Helper.php
+++ src/Helper.php
@@ -17,26 +17,27 @@ class Helper
   /**
    * Print logo in console
    */
-  public function printLogo()
+  public static function printLogo()
   {
-    echo '#  ###  ###     #         ### '.PHP_EOL ;
-    echo '#  #    #       #  #  #  #    '.PHP_EOL;
-    echo '#  ###  ##   ####  #  # #     '.PHP_EOL;
-    echo '#     # #    # ##  #  #  #    '.PHP_EOL;
-    echo '#  ###  ###  ####   ###   ### '.PHP_EOL;
-    echo 'Синхронизация данных с ИС ПП  '.PHP_EOL.PHP_EOL;
+    echo '#  ###  ###     #         ### ', PHP_EOL;
+    echo '#  #    #       #  #  #  #    ', PHP_EOL;
+    echo '#  ###  ##   ####  #  # #     ', PHP_EOL;
+    echo '#     # #    # ##  #  #  #    ', PHP_EOL;
+    echo '#  ###  ###  ####   ###   ### ', PHP_EOL;
+    echo 'Синхронизация данных с ИС ПП  ', PHP_EOL;
+    echo PHP_EOL;
   }
 
   /**
    * Print help
    */
-  public function printHelp()
+  public static function printHelp()
   {
-    echo 'Ошибка параметра, воспользуйтесь справкой: '.PHP_EOL;
-    echo 'groups      Синхронизация групп пользователей'.PHP_EOL;
-    echo 'students    Синхронизация обучающихся в данном отделении'.PHP_EOL;
-    echo 'events      Синхронизация событий в данном отделении'.PHP_EOL;
-    echo 'all         Последовательная синхронизация групп, обучающихся и событий в данном отделении'.PHP_EOL;
+    echo 'Ошибка параметра, воспользуйтесь справкой: ', PHP_EOL;
+    echo 'groups      Синхронизация групп пользователей', PHP_EOL;
+    echo 'students    Синхронизация обучающихся в данном отделении', PHP_EOL;
+    echo 'events      Синхронизация событий в данном отделении', PHP_EOL;
+    echo 'all         Последовательная синхронизация групп, обучающихся и событий в данном отделении', PHP_EOL;
     echo PHP_EOL;
   }
 }
