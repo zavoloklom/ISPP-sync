61d43bb2d37ed6e1299643cd5c709a2a927dd777
Sergey Kupletsky <s.kupletsky@gmail.com>
2017-03-20 04:28:49 +0300

    Update main file

--- sync.php
+++ sync.php
@@ -4,6 +4,9 @@
  * Console bootstrap file.
  */
 
+use zavoloklom\ispp\sync\src\Helper;
+use zavoloklom\ispp\sync\src\Synchronization;
+
 set_time_limit(0);
 //error_reporting(E_ALL & ~E_NOTICE);
 date_default_timezone_set("Europe/Moscow");
@@ -11,30 +14,21 @@ date_default_timezone_set("Europe/Moscow");
 defined('STDIN') or define('STDIN', fopen('php://stdin', 'r'));
 defined('STDOUT') or define('STDOUT', fopen('php://stdout', 'w'));
 
-require(__DIR__ . '/vendor/autoload.php');
+require_once(__DIR__ . '/vendor/autoload.php');
 
 // Load configuration
 $config = include_once('config.php');
 
 // ASCII Logotype
-$helper = new \zavoloklom\ispp\sync\src\Helper();
-$helper->printLogo();
-
-// Approved commands and options
-$actions = [
-  'groups',
-  'students',
-  'events',
-  'all'
-];
-$options = [];
+Helper::printLogo();
 
 // Проверка корректности действий и установка действия по умолчанию
-if ($argc >= 2 && in_array($argv[1], $actions)) {
+if ($argc >= 2 && in_array($argv[1], Synchronization::actionsArray())) {
   $action = $argv[1];
-  // Make connection
-  $connection = new \zavoloklom\ispp\sync\src\Synchronization($config);
-  $connection->$action();
+  $app = new Synchronization($config);
+  //$app->setupConnection();
+  //$app->execute($action, $params)
+  $app->$action();
 } else {
-  $helper->printHelp();
+  Helper::printHelp();
 }
