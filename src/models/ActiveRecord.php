63bd3bccda1d2c91605d156fe244dd148ced30df
Sergey Kupletsky <s.kupletsky@gmail.com>
2017-03-21 17:01:02 +0300

    Create ActiveRecord

---
+++ src/models/ActiveRecord.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017 Sergey Kupletsky
+ * @license MIT
+ * @link https://github.com/zavoloklom/ISPP-sync
+ */
+
+namespace zavoloklom\ispp\sync\src\models;
+
+/**
+ * Class ActiveRecord
+ * @package zavoloklom\ispp\sync\src\models
+ */
+class ActiveRecord
+{
+
+  private $config;
+
+  public function __construct()
+  {
+    $this->config;
+  }
+}
