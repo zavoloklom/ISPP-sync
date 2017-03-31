86de43269ca37b5710a9626db01b6f1c8b1066c6
Sergey Kupletsky <s.kupletsky@gmail.com>
2017-03-31 05:10:36 +0300

    Create IsppGroup models

---
+++ src/models/IsppGroupQuery.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017 Sergey Kupletsky
+ * @license MIT
+ * @link https://github.com/zavoloklom/ISPP-sync
+ */
+
+namespace zavoloklom\ispp\sync\src\models;
+
+use Pixie\QueryBuilder\QueryBuilderHandler;
+
+/**
+ * Class IsppGroupQuery
+ *
+ * @package zavoloklom\ispp\sync\src\models
+ */
+class IsppGroupQuery extends QueryBuilderHandler
+{
+  public function active()
+  {
+    return $this->where(IsppGroup::tableName().'.state', '=', IsppGroup::STATE_ACTIVE);
+  }
+
+  public function inactive()
+  {
+    return $this->where(IsppGroup::tableName().'.state', '=', IsppGroup::STATE_INACTIVE);
+  }
+
+}
