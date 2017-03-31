007efa1a430842043bf90f6167ef3ec1110d8125
Sergey Kupletsky <s.kupletsky@gmail.com>
2017-04-01 01:43:05 +0300

    Add Client models

---
+++ src/models/ClientQuery.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017 Sergey Kupletsky
+ * @license MIT
+ * @link https://github.com/zavoloklom/ISPP-sync
+ */
+
+namespace zavoloklom\ispp\sync\src\models;
+
+use Pixie\QueryBuilder\QueryBuilderHandler;
+
+/**
+ * Class ClientQuery
+ *
+ * @package zavoloklom\ispp\sync\src\models
+ */
+class ClientQuery extends QueryBuilderHandler
+{
+  public function schoolClasses()
+  {
+    return $this->where(ClientsGroup::tableName().'.GroupType', '=', ClientsGroup::TYPE_CLASS);
+  }
+
+  public function kindergartens()
+  {
+    return $this->where(ClientsGroup::tableName().'.GroupType', '=', ClientsGroup::TYPE_KINDERGARTEN);
+  }
+
+}
