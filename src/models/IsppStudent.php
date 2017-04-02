fbd61c961c290917bf36b13dc7a3887cfe42d3af
Sergey Kupletsky <s.kupletsky@gmail.com>
2017-04-02 16:50:47 +0300

    Add Ispp Student models

---
+++ src/models/IsppStudent.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017 Sergey Kupletsky
+ * @license MIT
+ * @link https://github.com/zavoloklom/ISPP-sync
+ */
+
+namespace zavoloklom\ispp\sync\src\models;
+
+use Pixie\Connection;
+
+/**
+ * Class IsppStudent
+ *
+ * @package zavoloklom\ispp\sync\src\models
+ */
+class IsppStudent extends ActiveRecord
+{
+
+  const STATE_INACTIVE  = 0;
+  const STATE_ACTIVE    = 1;
+
+  /**
+   * @inheritdoc
+   */
+  public static function getConnection()
+  {
+    return new Connection(CONFIG['web_server']['adapter'], CONFIG['web_server']['options']);
+  }
+
+  /**
+   * @inheritdoc
+   * @return IsppStudentQuery
+   */
+  public static function qb()
+  {
+    $qb = new IsppStudentQuery(self::getConnection());
+    return $qb->table(self::tableName());
+  }
+
+}
---
+++ src/models/IsppStudentQuery.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017 Sergey Kupletsky
+ * @license MIT
+ * @link https://github.com/zavoloklom/ISPP-sync
+ */
+
+namespace zavoloklom\ispp\sync\src\models;
+
+use Pixie\QueryBuilder\QueryBuilderHandler;
+
+/**
+ * Class IsppStudentQuery
+ *
+ * @package zavoloklom\ispp\sync\src\models
+ */
+class IsppStudentQuery extends QueryBuilderHandler
+{
+  public function active()
+  {
+    return $this->where(IsppStudent::tableName().'.state', '=', IsppStudent::STATE_ACTIVE);
+  }
+
+  public function inactive()
+  {
+    return $this->where(IsppStudent::tableName().'.state', '=', IsppStudent::STATE_INACTIVE);
+  }
+
+}
