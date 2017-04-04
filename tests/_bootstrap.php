e85b587c8df8050739892a39df496b4a24a7f5b4
Sergey Kupletsky <s.kupletsky@gmail.com>
2017-04-04 09:10:13 +0300

    Update dump files and how implement them

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,7 +1,9 @@
 <?php
-use Codeception\Util\Fixtures;
-
 define('CONFIG', include_once(__DIR__ . '\config.php'));
 
-// Fixtures::add('config', $config);
-// Fixtures::add('sync_class', new \zavoloklom\ispp\sync\src\Synchronization($config));
+// Применить ecafe_dump
+if (CONFIG['local_server']['adapter'] == 'mysql') {
+  $dumpConfig = CONFIG['local_server']['options'];
+  $password = $dumpConfig['password'] ? '-p '.$dumpConfig['password'] : '';
+  exec('mysql -h '.$dumpConfig['host'].' -u '.$dumpConfig['username'].' '.$password.' < '.__DIR__.'/_data/ecafe_dump.sql');
+}
